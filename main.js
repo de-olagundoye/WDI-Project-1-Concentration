5e8181856e5d9197c07eee493b371b64b8d09f00
De Olagundoye <de.olagundoye@gmail.com>
2015-11-09 11:27:55 -0500

    got event listeners working again

--- main.js
+++ main.js
@@ -21,41 +21,74 @@ $(document).ready(function(){
 
 	var handleClick = function (event){
 		var $theCard = $(event.target);
-		console.log(event.target)
-		
-		if ($theCard = $card1) {
+
+		if ($theCard.attr('id') === $card1.attr('id')) {
 			console.log("1");
 
-		} else if ($theCard = $card2) {
+		} else if ($theCard.attr('id') === $card2.attr('id')) {
 			console.log("2");
 
-		} else if ($theCard = $card3) {
-			console.log("is");
+		} else if ($theCard.attr('id') === $card3.attr('id')) {
+			console.log("3");
 
-		} else if ($theCard = $card4) {
-			console.log("this");
+		} else if ($theCard.attr('id') === $card4.attr('id')) {
+			console.log("4");
 
-		} else if ($theCard = $card5) {
-			console.log("even");
+		} else if ($theCard.attr('id') === $card5.attr('id')) {
+			console.log("5");
 
-		} else if ($theCard = $card6) {
-			console.log("working");
+		} else if ($theCard.attr('id') === $card6.attr('id')) {
+			console.log("6");
 
-		} else if ($theCard = $card7) {
-			console.log("no?");
+		} else if ($theCard.attr('id') === $card7.attr('id')) {
+			console.log("7");
 
-		} else if ($theCard = $card8) {
-			console.log("omg.");
+		} else if ($theCard.attr('id') === $card8.attr('id')) {
+			console.log("8");
+
+		} else if ($theCard.attr('id') === $card9.attr('id')) {
+			console.log("9");
+
+		} else if ($theCard.attr('id') === $card10.attr('id')) {
+			console.log("10");
+
+
+		} else if ($theCard.attr('id') === $card11.attr('id')) {
+			console.log("11");
+
+		} else if ($theCard.attr('id') === $card12.attr('id')) {
+			console.log("12");
+
+		} else if ($theCard.attr('id') === $card13.attr('id')) {
+			console.log("13");
+
+		} else if ($theCard.attr('id') === $card14.attr('id')) {
+			console.log("14");
+
+		} else if ($theCard.attr('id') === $card15.attr('id')) {
+			console.log("15");
+
+		} else if ($theCard.attr('id') === $card16.attr('id')) {
+			console.log("16");
 		}
 	};
 
 		$card1.on('click', handleClick);
 		$card2.on('click', handleClick);
 		$card3.on('click', handleClick);
-		// $card4.on('click', handleClick);
-		// $card5.on('click', handleClick);
-		// $card6.on('click', handleClick);
-		// $card7.on('click', handleClick);
-		// $card8.on('click', handleClick);
+		$card4.on('click', handleClick);
+		$card5.on('click', handleClick);
+		$card6.on('click', handleClick);
+		$card7.on('click', handleClick);
+		$card8.on('click', handleClick);
+		$card9.on('click', handleClick);
+		$card10.on('click', handleClick);
+		$card11.on('click', handleClick);
+		$card12.on('click', handleClick);
+		$card13.on('click', handleClick);
+		$card14.on('click', handleClick);
+		$card15.on('click', handleClick);
+		$card16.on('click', handleClick);
+
 
 	});
